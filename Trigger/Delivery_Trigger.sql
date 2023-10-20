2af32b337fbf384688333b442a55b9e694301b20
Egorov050 <115251460+Egorov050@users.noreply.github.com>
2023-10-20 21:19:10 +0300

    Update Delivery_Trigger.sql

--- Trigger/Delivery_Trigger.sql
+++ Trigger/Delivery_Trigger.sql
@@ -1,5 +1,9 @@
+/*Тригер атоматически добавляет данные о зп курьера за заказ(используется функция, которая это все суммирует)*/
 CREATE TRIGGER delivery_time_trigger
 AFTER UPDATE OF delivery_end ON delivery
 FOR EACH ROW
 WHEN (NEW.delivery_time IS NOT NULL)
 EXECUTE FUNCTION update_courier_delivery_time();
+
+
+
